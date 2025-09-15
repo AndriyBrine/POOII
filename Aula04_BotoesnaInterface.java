fe8502bd1c1c9cb96d7c834ac52e573ec5da70a3
AndriyBrine <163847323+AndriyBrine@users.noreply.github.com>
2025-09-15 08:45:25 -0300

    Update and rename Aula04.java to Aula04_BotoesnaInterface.java

--- Aula04_BotoesnaInterface.java
+++ Aula04_BotoesnaInterface.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
-/**
- *
- * @author 20241tiimi0062
- */
-public class Aula04 extends javax.swing.JFrame {
+public class Aula04_BotoesnaInterface extends javax.swing.JFrame {
 
     /**
      * Creates new form Aula03
      */
-    public Aula04() {
+    public Aula04_BotoesnaInterface() {
         initComponents();
     }
 
