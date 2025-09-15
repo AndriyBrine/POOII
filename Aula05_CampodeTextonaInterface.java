1e6757a60395572171d7a904f8da4621a37609fa
AndriyBrine <163847323+AndriyBrine@users.noreply.github.com>
2025-09-15 08:47:15 -0300

    Update and rename Aula05.java to Aula05_CampodeTextonaInterface.java

--- Aula05_CampodeTextonaInterface.java
+++ Aula05_CampodeTextonaInterface.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
-/**
- *
- * @author 20241tiimi0062
- */
-public class Aula05 extends javax.swing.JFrame {
+public class Aula05_CampodeTextonaInterface extends javax.swing.JFrame {
 
     /**
      * Creates new form Aula05
      */
-    public Aula05() {
+    public Aula05_CampodeTextonaInterface() {
         initComponents();
     }
 
