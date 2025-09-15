2b3f327f0d5e7ac1b2a868a2341b6f9bb987100f
AndriyBrine <163847323+AndriyBrine@users.noreply.github.com>
2025-09-15 08:48:04 -0300

    Update and rename Aula06.java to Aula06_ImagemnaInterface.java

--- Aula06_ImagemnaInterface.java
+++ Aula06_ImagemnaInterface.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
-/**
- *
- * @author 20241tiimi0062
- */
-public class Aula06 extends javax.swing.JFrame {
+public class Aula06_ImagemnaInterface extends javax.swing.JFrame {
 
     /**
      * Creates new form Aula06
      */
-    public Aula06() {
+    public Aula06_ImagemnaInterface() {
         initComponents();
     }
 
